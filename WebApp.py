23ef2cb6383bc0649c3a3b65c4d96f5f86022b6b
Stephen-LiuHan <stephensugimoto@gmail.com>
2021-05-23 00:02:21 +0900

    change app() arg

--- WebApp.py
+++ WebApp.py
@@ -2,19 +2,24 @@ import os
 
 from flask import Flask, render_template
 
-from FaceIDoor import *
+#from FaceIDoor import *
 
 app = Flask(__name__)
 
-TEMPLATE = os.path.join(WORKING_DIR, "Template/")
+#WORKING_DIR = os.path.dirname(__file__)
+#print(__file__)
+#TEMPLATE = os.path.join(WORKING_DIR, "templates")
+
+
 
 @app.route('/')
 def auth():
-    return render_template(os.path.join(TEMPLATE, "auth.html"))
+    return render_template("Auth.html")
 
-@app.@app.route('/Auth')
-def auth():
-    open()
+@app.route('/Auth')
+def auth_myself():
+    #open()
+    pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port=5000)
