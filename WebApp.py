f2ce54f159918edb161259b360bb78224670b079
Stephen-LiuHan <stephensugimoto@gmail.com>
2021-05-31 13:20:19 +0900

    add digestauth

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template
+from flask_httpauth import HTTPDigestAuth
 
 from FaceIDoor import *
 
@@ -8,11 +9,23 @@ app = Flask(__name__)
 
 TEMPLATE = os.path.join(WORKING_DIR, "Template/")
 
+users = {
+    "User" : "FaceIDoor",
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 @app.route('/')
+@auth.login_required
 def auth():
     return render_template(os.path.join(TEMPLATE, "auth.html"))
 
-@app.@app.route('/Auth')
+@auth.login_required
+@app.route('/Auth')
 def auth():
     open()
 
